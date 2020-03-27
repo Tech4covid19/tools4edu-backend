3f343a896985c7bc41209b5da1daf0bd2d310d1b
rui mendes <ruitiagomendes@gmail.com>
2020-03-27 22:58:53 +0000

    serverless

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { NestExpressApplication, ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
+const expressApp = express();
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter(expressApp));
+  await app.listen(3000);
 
   if (module.hot && !process.env.production) {
     module.hot.accept();
@@ -15,3 +17,4 @@ async function bootstrap() {
 
 }
 bootstrap();
+
