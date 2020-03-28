ae868c5225d5a7bb0d7daffe68d140297b7a20aa
Joao Rocha <jsfrocha@gmail.com>
2020-03-28 04:04:48 +0000

    Filter by Stakeholder under Videos

--- src/providers/providers.resolver.ts
+++ src/providers/providers.resolver.ts
@@ -2,6 +2,8 @@ import { Args, ID, Parent, Query, ResolveField, ResolveProperty, Resolver } from
 import { ProvidersService } from './providers.service';
 import { Provider } from './models/provider.model';
 import { Video } from '../videos/models/video.model';
+import { filter } from 'rxjs/operators';
+import { Video as IVideo } from '../videos/interfaces/video.interface';
 
 @Resolver(of => Provider)
 export class ProvidersResolver {
@@ -15,8 +17,21 @@ export class ProvidersResolver {
   }
 
   @ResolveField('videos', type => [Video], {})
-  async resolveVideos(@Parent() provider: Provider) {
-    return this.providersService.getVideosForProvider(provider.code)
+  async resolveVideos(
+    @Parent() provider: Provider,
+    @Args('stakeholder', { nullable: true }) stakeholderCode: string
+  ) {
+    let filteredVideos = [];
+
+    const providerVideos = await this.providersService.getVideosForProvider(provider.code, !!stakeholderCode);
+
+    if (stakeholderCode) {
+      filteredVideos = providerVideos.filter(video => video.stakeholder['code'] === stakeholderCode)
+    } else {
+      filteredVideos = [].concat(providerVideos);
+    }
+
+    return Promise.resolve(filteredVideos)
   }
 
   //
