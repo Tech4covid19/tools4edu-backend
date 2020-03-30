13e1098d850c704cdae6236f23a2eccc690f6f9c
Joao Rocha <jsfrocha@gmail.com>
2020-03-30 03:10:59 +0100

    Video added "time" property;

--- src/videos/interfaces/video.interface.ts
+++ src/videos/interfaces/video.interface.ts
@@ -3,6 +3,7 @@ import { Document, Schema } from 'mongoose';
 export interface Video extends Document {
   readonly order: number;
   readonly videoUrl: string;
+  readonly time: string;
   readonly title: string;
   readonly description: string;
   readonly stakeholder: Schema.Types.ObjectId;
